7705d144d95e90856bd2f6e376de405f64a60493
tonantr <tonantruong@hotmail.com>
2023-03-13 22:59:17 -0400

    complete Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser = "string";
+
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser} ${"!"}`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()} ${"!"}`;
+
+const firstInitial = currentUser[0];
+
+const shortGreeting = `Welcome, ${firstInitial} ${"!"}`;
